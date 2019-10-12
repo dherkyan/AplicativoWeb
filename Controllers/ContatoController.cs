55597c8aaf7eeeda4a6f759f2c4ee00d817b876a
Dherkyan Ribeiro <dherkyan@gmail.com>
2019-10-11 20:21:31 -0400

    Inicio Aplicativo Web

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AplicativoWeb.Controllers
+{
+    public class ContatoController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
